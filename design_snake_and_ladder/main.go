12b1771da5a7fa187e78a1d4aa5152f0c7fa3edb
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2025-08-12 17:39:12 +0530

    driver code

--- design_snake_and_ladder/main.go
+++ design_snake_and_ladder/main.go
@@ -1,2 +1,44 @@
 package main
 
+import (
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	rand.Seed(time.Now().UnixNano())
+
+	snakes := []Snake{
+		{Head: 14, Tail: 7},
+		{Head: 31, Tail: 20},
+		{Head: 38, Tail: 5},
+		{Head: 84, Tail: 28},
+		{Head: 97, Tail: 78},
+	}
+
+	ladders := []Ladder{
+		{Start: 3, End: 22},
+		{Start: 5, End: 8},
+		{Start: 11, End: 26},
+		{Start: 20, End: 29},
+		{Start: 27, End: 56},
+	}
+
+	board := NewBoard(100, snakes, ladders)
+
+	players := []*Player{
+		{Name: "Alice", Position: 0},
+		{Name: "Bob", Position: 0},
+	}
+
+	dice := &Dice{Sides: 6}
+
+	game := &Game{
+		Players: players,
+		Board:   board,
+		Dice:    dice,
+	}
+
+	game.Play()
+}
